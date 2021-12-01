4a6667f69380f1afa3740b0bfd045b3791e7330c
Kovács József <szemurkaloz@gmail.com>
2021-12-01 14:46:56 +0100

    Navbar Készenvan

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectCard from "../components/ProjectCard";
-import { projects } from "../data";
+import ProjectNavbar from "../components/ProjectNavbar";
+import { projects as ProjectsData } from "../data";
+import { Category } from "../type"
 
 function Projects() {
+const [projects, setprojects] = useState(ProjectsData);
+const [active, setActive] = useState("All")
+
+const handelFilterCategory = (category: Category | "All") => {
+    if (category === "All") {
+        setprojects(ProjectsData);
+        setActive(category);
+        return
+    }
+
+    const newArray = ProjectsData.filter((project) => project.category.includes(category));
+    setprojects(newArray);
+    setActive(category);
+}
+
   return (
     <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
-      <nav>Navbar</nav>
+      <ProjectNavbar handlerFilterCategory={handelFilterCategory} active={active}/>
       <div className="relative grid grid-cols-12 gap-4 my-3">
       {projects.map((project) => (
           <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200" key={project.name}>
